999ac4a8db751afbd144286b016d8b159cff9c0a
mafazans <mafazans>
2017-11-02 19:02:59 +0700

    update footer & nav as par

--- resources/views/homepage/index.blade.php
+++ resources/views/homepage/index.blade.php
@@ -4,18 +4,7 @@
 
         <div class='preloader'><div class='loaded'>&nbsp;</div></div>
 
-
-        <nav id="main-nav">
-            <ul>
-                <li><a href="#home"><span>Home</span></a></li>
-                <li><a href="#about"><span>About Me</span></a></li>
-                <li><a href="#features"><span>Features</span></a></li>
-                <li><a href="#experience"><span>Experience</span></a></li>
-                <li><a href="#portfolio"><span>Portfolio</span></a></li>
-
-            </ul>
-            <a href="#0" class="cd-close-menu">Close<span></span></a>
-        </nav>
+@include('nav')
 
         <!--Home page style-->
         <header id="home" class="home home-main-content">
---
+++ resources/views/nav.blade.php
@@ -0,0 +1,11 @@
+        <nav id="main-nav">
+            <ul>
+                <li><a href="#home"><span>Home</span></a></li>
+                <li><a href="#about"><span>About Me</span></a></li>
+                <li><a href="#features"><span>Features</span></a></li>
+                <li><a href="#experience"><span>Experience</span></a></li>
+                <li><a href="#portfolio"><span>Portfolio</span></a></li>
+
+            </ul>
+            <a href="#0" class="cd-close-menu">Close<span></span></a>
+        </nav>
