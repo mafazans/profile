2ddea977411b4c72f6d5a2f431d65ee23d860651
mafazans <mafazans>
2017-11-02 18:18:45 +0700

    first commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<head>
+    @include('head')
+</head>
+    <body>
+    @yield('content')
+
+
+        <!--Footer-->
+        <footer id="footer" class="footer">
+            <div class="container text-center">
+                <div class="row">
+                    <div class="col-md-6 col-md-offset-3 col-sm-12 col-xs-12">
+                        <div class="main-footer">
+
+                            <div class="social">
+                                <a href="#"><i class="fa fa-twitter"></i></a>
+                                <a href="#"><i class="fa fa-linkedin"></i></a>
+                                <a href="#"><i class="fa fa-behance"></i></a>
+                                <a href="#"><i class="fa dribbble fa-dribbble"></i></a>
+                            </div>
+
+                            <p>Made with <i class="fa fa-heart"></i> by <a target="_blank" href="http://bootstrapthemes.co"> Bootstrap Themes </a>2016. All rights reserved.</p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </footer>
+
+
+        <script src="{{ asset('js/vendor/jquery-1.11.2.min.js') }}"></script>
+        <script src="{{ asset('js/vendor/bootstrap.min.js') }}"></script>
+
+        <script src="{{ asset('js/plugins.js') }}"></script>
+        <script src="{{ asset('js/jquery.magnific-popup.js') }}"></script>
+        <script src="{{ asset('js/modernizr.js') }}"></script>
+
+        <script src="{{ asset('js/main.js') }}"></script>
+    </body>
+</html>
